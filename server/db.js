6168e5d42b2a04fcb73afe00ff25b9670b852478
mattislub <166994172+mattislub@users.noreply.github.com>
2025-09-01 04:57:42 +0300

    feat: add postgres storage server

---
+++ server/db.js
@@ -0,0 +1,18 @@
+import pg from 'pg';
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+export async function init() {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS storage (
+      key text PRIMARY KEY,
+      data jsonb
+    )
+  `);
+}
+
+export function query(text, params) {
+  return pool.query(text, params);
+}
