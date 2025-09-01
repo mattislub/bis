ac318c0b9adffbd586236a9bef4c939bc59c316e
mattislub <166994172+mattislub@users.noreply.github.com>
2025-09-02 00:48:21 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: "api-seating",
+    cwd: "/root/Seating-management",
+    script: "server/index.js",
+    exec_mode: "fork",
+    env: {
+      NODE_ENV: "production",
+      SMTP_HOST: "smtp.hostinger.com",
+      SMTP_PORT: "465",
+      SMTP_USER: "info@seatflow.online",
+      SMTP_PASS: "613Ml#613"
+      SMTP_FROM="ניהול מושבים חכם<info@seatflow.online>"
+    }
+  }]
+}
